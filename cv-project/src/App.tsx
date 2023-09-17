4388c66603cf7fb0e992e74ff10dc37313e2850e
Frenzy017 <aleksladjov4@gmail.com>
2023-09-17 21:39:24 +0300

    Initial Commit

---
+++ cv-project/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+
+function App() {
+
+    //  Create personal details panel with JSX
+
+    return (
+        <div className="max-h-screen flex justify-around items-center">
+            <h1 className="text-3xl font-bold text-blue-600">
+               CV Application
+            </h1>
+        </div>
+    );
+}
+export default App;
