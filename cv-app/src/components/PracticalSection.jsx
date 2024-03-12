4645855e9d59d866c85deed237270b8e4ca4cfd6
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-03-12 20:02:12 +0200

    Create PracticalSection.jsx file

---
+++ cv-app/src/components/PracticalSection.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+
+import Input from "./Input"
+
+export default function PracticalSection({ title, name, positionTitle, achievements, dateWorkStartEnd }) {
+    return (
+        <div>
+            <section className="grid col-end-1 ml-4">
+                <div className="flex mb-8">{title}</div>
+                <form action="">
+                    <div>{name}
+                        <Input />
+                    </div>
+                    <div>{positionTitle}
+                        <Input />
+                    </div>
+                    <div>{achievements}
+                        <Input />
+                    </div>
+                    <div>{dateWorkStartEnd}
+                        <Input />
+                    </div>
+                </form>
+            </section>
+        </div>
+    )
+}
