e80a8d2cbc3d46de33992eb120452461f6535889
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-03-13 20:48:56 +0200

    Add font styles

--- cv-app/tailwind.config.js
+++ cv-app/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto": ["Roboto", "sans-serif"],
+        "open-sans": ["Open Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
