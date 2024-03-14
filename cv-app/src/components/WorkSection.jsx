62c683c687fc40cdc47bbdaad49e783e767a6dad
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-03-14 21:42:29 +0200

    Update input design

---
+++ cv-app/src/components/WorkSection.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable react/prop-types */
+
+// import Input from "./Input"
+
+export default function Work({ title, companyName, jobTitle, workStartDate, workEndDate, achievements }) {
+
+
+
+    return (
+        <div>
+            <div>
+                <section className="flex justify-start p-3">
+                    <form className="bg-slate-600 p-2 text-white text-base font-robot border-2 border-white rounded-md">
+
+                        <span className="flex justify-center pt-1.5 mb-3 font-open-sans italic font-semibold text-lg">{title}</span>
+
+                        <div className="flex justify-start gap-4">
+                            <div>{jobTitle}
+                                <input
+                                    type="text" placeholder="Intern Audit"
+                                    className="flex pl-1 rounded-md max-w-44 text-black
+                                    focus:border-black border-2 border-solid focus:outline-none place"
+                                />
+                            </div>
+                            <div>{companyName}
+                                <input
+                                    type="text" placeholder="Morgan Stanley"
+                                    className="flex pl-1 rounded-md max-w-44 text-black
+                                    focus:border-black border-2 border-solid focus:outline-none place"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col pt-3">{achievements}
+                            <input
+                                type="text" placeholder="Signed up more than 200 enterprise clients"
+                                className="pl-1 flex-grow-1 rounded-md min-w-full text-black focus:border-black border-2 border-solid focus:outline-none place" />
+                            <button className="self-end absolute pt-6 pr-1">➕</button>
+                        </div>
+
+
+                        <div className="flex justify-center items-center pt-1 gap-4">
+                            <div className="text-center">{workStartDate}
+                                <input
+                                    type="date"
+                                    className="flex pl-1 rounded-md max-w-44 text-black focus:border-black border-2 border-solid focus:outline-none place" />
+                            </div>
+
+
+                            <div className="text-center">{workEndDate}
+                                <input
+                                    type="date"
+                                    className="flex pl-1 rounded-md max-w-44 text-black focus:border-black border-2 border-solid focus:outline-none place" />
+                            </div>
+                        </div>
+                    </form>
+                </section>
+            </div>
+
+
+        </div>
+    )
+}
