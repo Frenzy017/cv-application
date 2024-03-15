83fc6f7b9a1d699ca012ae7785e1e0ce0cd3c676
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-03-15 19:04:38 +0200

    Create CvSection.jsx

---
+++ cv-app/src/components/CvSection.jsx
@@ -0,0 +1,21 @@
+export default function CvSection() {
+    return (
+
+        <div className="grid">
+            <section className="
+            max-w-3xl
+            h-1/1 mr-8 bg-slate-100
+            border-4 border-slate-700
+            shadow-2xl shadow-slate-700
+            "
+            >
+
+                <div className="grid grid-cols-2 grid-rows-2">
+                    <span className="bg-slate-300 col-span-2">Hello there</span>
+                    <span className="bg-slate-600">Hello</span>
+                </div>
+            </section>
+        </div>
+
+    )
+}
