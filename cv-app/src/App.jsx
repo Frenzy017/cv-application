78a3322de2a92e8462e7aeafc3ddcc70612ffd94
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-03-20 20:56:22 +0200

    Fixed section's height no more overflow

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,43 +1,47 @@
 /* eslint-disable react/prop-types */
 
-import EducationalSection from "./components/EducationalSection"
+import CvSection from "./components/CvSection"
+import EducationSection from "./components/EducationSection"
 import GeneralSection from "./components/GeneralSection"
-import PracticalSection from "./components/PracticalSection"
-
+import WorkSection from "./components/WorkSection"
 
 function App() {
 
   return (
-    <div className="grid grid-cols-2">
-      <div
-        className="flex justify-center col-start-2 row-span-2 h-1/1 mr-8 pl-8 bg-purple-300 border-4 border-slate-700" >
-        CV Template
-      </div>
+    <div>
+      <div className="grid grid-cols-2">
+        <div className="bg-slate-800 max-w-fit p-3 shadow-2xl h-fit shadow-slate-700 rounded-xl">
+          <GeneralSection
+            title="General Information"
+            name="Name"
+            lastName="Last Name"
+            email="Email"
+            phoneNumber="Phone number"
+          />
+          <EducationSection
+            title="Education Experience"
+            name="School name"
+            titleOfStudy="Title of study"
+            achievements="Achievements / Tasks"
+            studyStartDate="Start Date"
+            studyEndDate="End Date"
+          />
+          <WorkSection
+            title="Work Experience"
+            companyName="Company name"
+            jobTitle="Job"
+            achievements="Achievements / Tasks"
+            workStartDate="Start date"
+            workEndDate="End date"
+          />
+        </div>
 
-      <div className="bg-slate-400 w-1/2">
-      
-        <GeneralSection
-          title="General Information"
-          name="Name"
-          lastName="Last Name"
-          email="Email"
-          phoneNumber="Phone number"
-        />
+        <CvSection />
 
-        <EducationalSection
-          title="Educational Section"
-          name="School name"
-          titleOfStudy="Title of study"
-          dateOfStudy="Date of study"
-        />
 
-        <PracticalSection
-          title="Practical Experience"
-          positionTitle="Positional title"
-          name="Company name"
-          achievements="Achievements / Tasks"
-          dateWorkStartEnd="Work Start / End"
-        />
+        {/* <footer className="flex justify-center mt-20">
+          Created by Aleksandar Ladzhov
+        </footer> */}
 
       </div>
     </div>
