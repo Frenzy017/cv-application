0dbfb5d2b302ee6073bffa9e2ec4fd3eeeaed38a
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-03-25 17:48:31 +0200

    Update Work section with new states

--- cv-app/src/components/WorkSection.jsx
+++ cv-app/src/components/WorkSection.jsx
@@ -4,11 +4,11 @@
 
 export default function WorkSection
     (
-        { title, companyName, jobTitle, achievements,
-            workStartDate, workEndDate,
-            jobChangeTitle, handleJobTitleChange,
-            companyTitle, handleCompanyTitleChange,
-            achievementsChange, handleAchievementsChange, setAchievementList, achievementList, nextId
+        { title, companyName, jobTitle, achievements, workStartDate, workEndDate,
+            jobChangeTitle, handleJobTitleChange, companyTitle, handleCompanyTitleChange,
+            achievementsChange, handleAchievementsChange, setAchievementList, achievementList,
+            nextId, setAchievementsChange, dateStartJob, handleDateStartJobChange,
+            dateEndJob, handleDateEndJobChange
         }
     ) {
 
@@ -39,15 +39,28 @@ export default function WorkSection
 
                         <div className="flex flex-col pt-3">{achievements}
                             <input
-                                type="text" placeholder="Signed up more than 200 enterprise clients" value={achievementsChange} onChange={handleAchievementsChange}
+                                type="text" placeholder="Signed up more than 200 enterprise clients" value={achievementsChange} onChange={handleAchievementsChange} required
                                 className="pl-1 flex-grow-1 rounded-md min-w-full text-black focus:border-black border-2 border-solid focus:outline-none place" />
+
                             <button className="self-end absolute pt-6 pr-1" onClick={(e) => {
-                                e.preventDefault;
+                                e.preventDefault();
+
+                                if (achievementsChange === "") {
+                                    alert("Please type something first!");
+                                    return;
+                                }
+
+                                // Creating an object for achievements 
+                                // achievementsChange is the input value 
+                                // We spread syntax the object to create new values
 
                                 setAchievementList([
                                     ...achievementList,
                                     { id: nextId++, name: achievementsChange }
                                 ]);
+
+                                // Resetting input value after an achievement is added
+                                setAchievementsChange("");
                             }}>➕</button>
                         </div>
 
@@ -55,14 +68,14 @@ export default function WorkSection
                         <div className="flex justify-center items-center pt-1 gap-4">
                             <div className="text-center">{workStartDate}
                                 <input
-                                    type="date"
+                                    type="date" value={dateStartJob} onChange={handleDateStartJobChange}
                                     className="flex pl-1 rounded-md max-w-44 text-black focus:border-black border-2 border-solid focus:outline-none place" />
                             </div>
 
 
                             <div className="text-center">{workEndDate}
                                 <input
-                                    type="date"
+                                    type="date" value={dateEndJob} onChange={handleDateEndJobChange}
                                     className="flex pl-1 rounded-md max-w-44 text-black focus:border-black border-2 border-solid focus:outline-none place" />
                             </div>
                         </div>
